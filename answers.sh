f95138f78e1ac7990e775f767168b9ad6ef26f4b
louai <louaibaghdadi27@gmail.com>
2023-09-17 17:35:08 +0300

    Basic Queries

---
+++ answers.sh
@@ -0,0 +1,22 @@
+\\\\ basic query \\\\\\\
+
+"Select Name from students"
+
+"Select * from students where Age>30"
+
+"SELECT * FROM students
+WHERE Gender="F" AND Age>30"
+
+"SELECT Points FROM students
+WHERE name="Alex""
+
+"INSERT INTO students (Name,Age,Gender,Points)
+VALUES ('Louai',20,"M",600);"
+
+"UPDATE students
+SET Points=Points+100
+WHERE name="Basma""
+
+"UPDATE students
+SET Points=Points-100
+WHERE name="Alex""

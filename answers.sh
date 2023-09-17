a9ca8eb3107502c3ffcdbba2be0c4524d7134b61
louai <louaibaghdadi27@gmail.com>
2023-09-17 18:20:35 +0300

    Creating Table

--- answers.sh
+++ answers.sh
@@ -20,3 +20,29 @@ WHERE name="Basma""
 "UPDATE students
 SET Points=Points-100
 WHERE name="Alex""
+
+
+
+
+\\\\\\ creating tables \\\\\\\
+
+"CREATE TABLE graduates (
+    ID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    Name TEXT NOT NULL UNIQUE,
+	Age INTEGER,
+    Gender TEXT,
+    Points INTEGER,
+	Graduation TEXT
+);"
+
+"INSERT INTO graduates(name,Age,Gender,Points,Graduation)
+SELECT name,Age,Gender,Points,"08/09/2018"
+FROM students
+WHERE name="Layal";"
+
+"UPDATE graduates
+SET Graduation = '08/09/2018'
+WHERE name = 'Layal'"
+
+"DELETE FROM students
+WHERE name="Layal""
